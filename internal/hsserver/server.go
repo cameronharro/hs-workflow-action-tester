aba799ccdc468015aee2fa8323cd8f44dbccb798
cameronharro <103271364+cameronharro@users.noreply.github.com>
2026-03-14 08:49:04 -0500

    feat: Add initial request sending

--- internal/hsserver/server.go
+++ internal/hsserver/server.go
@@ -1,11 +1,66 @@
 package hsserver
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/cameronharro/hs-workflow-tester/internal/actiondefinition"
+	"github.com/cameronharro/hs-workflow-tester/internal/testcases"
+)
+
 type HSServer struct {
 	clientSecret string
+	actionURL    string
 }
 
-func NewHSServer(clientSecret string) HSServer {
-	return HSServer{
+func NewHSServer(clientSecret, actionURL string) *HSServer {
+	return &HSServer{
 		clientSecret: clientSecret,
+		actionURL:    actionURL,
 	}
+}
+
+func (s *HSServer) SendRequest(actionDef actiondefinition.ActionDefinition, testCase testcases.TestCase) error {
+	reqBody, err := createBody(actionDef, testCase)
+	if err != nil {
+		return err
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	method := "POST"
+	req, err := http.NewRequestWithContext(ctx, method, s.actionURL, reqBody)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("X-HubSpot-Signature", signRequestV2(s.clientSecret, method, s.actionURL, reqBody.String()))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(resBody))
+	return nil
+}
+
+func createBody(actionDef actiondefinition.ActionDefinition, testCase testcases.TestCase) (*bytes.Buffer, error) {
+	type Body struct {
+		ActionDef actiondefinition.ActionDefinition
+		TestCase  testcases.TestCase
+	}
+	body := Body{actionDef, testCase}
+	data, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBuffer(data), nil
 }

90bcb4c6ea0a38dc0a3d1df8b954b5ddbaa2114f
cameronharro <103271364+cameronharro@users.noreply.github.com>
2026-06-24 10:54:42 -0500

    test(jshelper): Extract PreAction tests into separate file

---
+++ internal/jshelper/runPreAction_test.go
@@ -0,0 +1,50 @@
+package jshelper_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/cameronharro/hs-workflow-tester/internal/jshelper"
+)
+
+func TestRunPreActionFunction(t *testing.T) {
+	type TestCase struct {
+		Name      string
+		Event     jshelper.PreActionEvent
+		Function  string
+		ExpectErr bool
+		ExpectVal jshelper.PreActionCallback
+	}
+	testCases := []TestCase{
+		{
+			Name: "Returns empty callback ",
+			Function: `exports.main = () => {
+				return {}
+			}`,
+			ExpectErr: true,
+		},
+		{
+			Name: "Returns valid callback",
+			Function: `exports.main = () => {
+				return {httpMethod: "POST"}
+			}`,
+			ExpectErr: false,
+			ExpectVal: jshelper.PreActionCallback{HttpMethod: jshelper.Post},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			result, err := jshelper.RunPreActionFunction(testCase.Event, testCase.Function)
+			if err != nil != testCase.ExpectErr {
+				if err == nil {
+					t.Fatal("Expected Error but got nil")
+				}
+				t.Fatal(err.Error())
+			}
+			if !reflect.DeepEqual(result, testCase.ExpectVal) {
+				t.Fatalf("Expected %s, got %s", testCase.ExpectVal, result)
+			}
+		})
+	}
+}

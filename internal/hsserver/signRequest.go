c106da75c946748da473482c3a9952efccdc72d0
cameronharro <103271364+cameronharro@users.noreply.github.com>
2026-06-24 10:57:15 -0500

    refactor(hsserver): Make body []byte type

--- internal/hsserver/signRequest.go
+++ internal/hsserver/signRequest.go
@@ -5,8 +5,8 @@ import (
 	"encoding/hex"
 )
 
-func signRequestV2(clientSecret, method, uri, body string) string {
-	s := clientSecret + method + uri + body
+func signRequestV2(clientSecret, method, uri string, body []byte) string {
+	s := clientSecret + method + uri + string(body)
 	h := sha256.New()
 	h.Write([]byte(s))
 	return hex.EncodeToString(h.Sum(nil))
--- internal/hsserver/signRequest_test.go
+++ internal/hsserver/signRequest_test.go
@@ -10,7 +10,7 @@ func TestSignRequest(t *testing.T) {
 		clientSecret string
 		method       string
 		uri          string
-		body         string
+		body         []byte
 		result       string
 	}
 
@@ -19,7 +19,7 @@ func TestSignRequest(t *testing.T) {
 			clientSecret: "fa768533-04ea-4d9b-becf-38cef1f96f0f",
 			method:       "POST",
 			uri:          "https://webhook.site/c5db800b-e3c3-4a16-af81-1e19f6d8b6a0",
-			body:         `{"callbackId":"ap-50467010-2384423471376-1-0","origin":{"portalId":50467010,"userId":null,"actionDefinitionId":259551814,"actionDefinitionVersion":1,"actionExecutionIndexIdentifier":{"enrollmentId":2384423471376,"actionExecutionIndex":0},"extensionDefinitionVersionId":1,"extensionDefinitionId":259551814},"context":{"workflowId":1791092093,"actionId":1,"actionExecutionIndexIdentifier":{"enrollmentId":2384423471376,"actionExecutionIndex":0},"source":"WORKFLOWS"},"object":{"objectId":153847279758,"objectType":"CONTACT"},"fields":{"label":"Brian Halligan (Sample Contact)","value":"153847279758"},"inputFields":{"label":"Brian Halligan (Sample Contact)","value":"153847279758"},"typedInputs":{"label":{"value":"Brian Halligan (Sample Contact)","type":"STRING"},"value":{"value":"153847279758","type":"STRING"}}}`,
+			body:         []byte(`{"callbackId":"ap-50467010-2384423471376-1-0","origin":{"portalId":50467010,"userId":null,"actionDefinitionId":259551814,"actionDefinitionVersion":1,"actionExecutionIndexIdentifier":{"enrollmentId":2384423471376,"actionExecutionIndex":0},"extensionDefinitionVersionId":1,"extensionDefinitionId":259551814},"context":{"workflowId":1791092093,"actionId":1,"actionExecutionIndexIdentifier":{"enrollmentId":2384423471376,"actionExecutionIndex":0},"source":"WORKFLOWS"},"object":{"objectId":153847279758,"objectType":"CONTACT"},"fields":{"label":"Brian Halligan (Sample Contact)","value":"153847279758"},"inputFields":{"label":"Brian Halligan (Sample Contact)","value":"153847279758"},"typedInputs":{"label":{"value":"Brian Halligan (Sample Contact)","type":"STRING"},"value":{"value":"153847279758","type":"STRING"}}}`),
 			result:       "d0c157dec62681b3ef5b9f7a1d66a9a63cfc67984b351a0f9d736e82ebdb0f03",
 		},
 	}
